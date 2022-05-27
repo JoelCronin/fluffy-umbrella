9350c582bcb72c83ffc8922815ed794da8ab21bc
joel cronin <you@example.com>
2022-05-27 17:16:36 -0400

    HTML and CSS basic skeleton formatted

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+var urlKey = "76488591069f094a172277faaa439f1e";
+var city = 
+
+
+// $( function() {
+//     var availableTags = [
+//       "ActionScript",
+//       "AppleScript",
+//       "Asp",
+//       "BASIC",
+//       "C",
+//       "C++",
+//       "Clojure",
+//       "COBOL",
+//       "ColdFusion",
+//       "Erlang",
+//       "Fortran",
+//       "Groovy",
+//       "Haskell",
+//       "Java",
+//       "JavaScript",
+//       "Lisp",
+//       "Perl",
+//       "PHP",
+//       "Python",
+//       "Ruby",
+//       "Scala",
+//       "Scheme"
+//     ];
+//     $( "#tags" ).autocomplete({
+//       source: availableTags
+//     });
+//   } );
