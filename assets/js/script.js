9db8e1f88969c42b95afeda7d27e0659e7cab0bd
joel cronin <you@example.com>
2022-05-29 13:35:55 -0400

    Current weather info populating in page when search button is pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,48 @@
 var urlKey = "76488591069f094a172277faaa439f1e";
-var city = 
+var city;
+var searchButton = document.getElementById('searchButton')
+var displayCity = document.getElementById('display-city')
+var displayEmoji = document.getElementById('display-emoji')
+var displayTemp = document.getElementById('display-temp')
+var displayWind = document.getElementById('display-wind')
+var displayUV = document.getElementById('display-UV')
+var weatherIcon = document.getElementById('weather-icon')
+
+searchButton.addEventListener('click', function(event){
+    event.preventDefault();
+    var city = document.getElementById("citySearch").value
+
+    if(city === ""){
+        console.log("blank")
+    } else{
+        console.log(city)
+        getCityAPi();
+    }
+    function getCityAPi(){
+        var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=76488591069f094a172277faaa439f1e";
+    
+        fetch(requestUrl)
+            .then(function(response){
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            displayCity.textContent = data.name
+            weatherIcon.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+            displayTemp.textContent = Math.round((data.main.temp - 273))
+            displayWind.textContent = data.wind.speed
+        })
+    }
+
+    function getFiveDayForecast(){
+        var secondRequestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=76488591069f094a172277faaa439f1e"
+    }
+})
 
 
-// $( function() {
-//     var availableTags = [
-//       "ActionScript",
-//       "AppleScript",
-//       "Asp",
-//       "BASIC",
-//       "C",
-//       "C++",
-//       "Clojure",
-//       "COBOL",
-//       "ColdFusion",
-//       "Erlang",
-//       "Fortran",
-//       "Groovy",
-//       "Haskell",
-//       "Java",
-//       "JavaScript",
-//       "Lisp",
-//       "Perl",
-//       "PHP",
-//       "Python",
-//       "Ruby",
-//       "Scala",
-//       "Scheme"
-//     ];
-//     $( "#tags" ).autocomplete({
-//       source: availableTags
-//     });
-//   } );
+
+
+console.log(city)
+
+
+
