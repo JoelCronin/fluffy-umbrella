33f2384c723b7b20f31b466e724d9903abc2a2ac
joel cronin <you@example.com>
2022-05-30 19:27:35 -0400

    Code refactored to make it more concise and HTML and CSS styling finalised

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,18 +11,14 @@ var weatherIcon = document.getElementById('weather-icon')
 var cityList = document.getElementById('city-list')
 var fivedayBox = document.getElementById("five-day-box")
 var secondSearch = document.getElementsByClassName("secondSearch")
+var weatherIcon = document.getElementById("weather-icon")
 
-
+//User Input into search box is used to get weather data from Open Weather when search button is pressed
 searchButton.addEventListener('click', function(event){
     event.preventDefault();
     var city = document.getElementById("citySearch").value
+    getCityAPi();
 
-    if(city === ""){
-        console.log("blank")
-    } else{
-        console.log(city)
-        getCityAPi();
-    }
 
     function getCityAPi(){
         var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=76488591069f094a172277faaa439f1e";
@@ -33,10 +29,10 @@ searchButton.addEventListener('click', function(event){
         })
         .then(function (data) {
             console.log(data);
-            const milliseconds = (data.dt * 1000)
-            const dateObject = new Date(milliseconds);
-            const dateFormat = dateObject.toLocaleString();
-            displayCity.textContent = data.name + " " + dateFormat
+            // This part populates all the current forecast HTML areas
+            weatherIcon.style.display = "block";
+            var dateString = moment.unix(data.dt).format("MM/DD/YYYY")
+            displayCity.textContent = data.name + " " + dateString
             weatherIcon.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
             displayTemp.textContent = "Temperature: " + Math.round((data.main.temp - 273)) + "c"
             displayWind.textContent = "Wind: " + data.wind.speed + "m/s";
@@ -47,68 +43,14 @@ searchButton.addEventListener('click', function(event){
             cityButton.addEventListener("click", nextSearch)
             cityList.append(cityButton);
             localStorage.setItem("data", JSON.stringify(data));
-            renderData();
-//Removes all old 5-day forecast elements to make way for new ones
-            var dateId0 = document.getElementById("dateId0");
-            dateId0.remove();
-            var dateId1 = document.getElementById("dateId1");
-            dateId1.remove();
-            var dateId2 = document.getElementById("dateId2");
-            dateId2.remove();
-            var dateId3 = document.getElementById("dateId3");
-            dateId3.remove();
-            var dateId4 = document.getElementById("dateId4");
-            dateId4.remove(); 
-
-            var imgId0 = document.getElementById("imgId0");
-            imgId0.remove();
-            var imgId1 = document.getElementById("imgId1");
-            imgId1.remove();
-            var imgId2 = document.getElementById("imgId2");
-            imgId2.remove();
-            var imgId3 = document.getElementById("imgId3");
-            imgId3.remove();
-            var imgId4 = document.getElementById("imgId4");
-            imgId4.remove();
-
-            var tempId0 = document.getElementById("tempId0");
-            tempId0.remove();
-            var tempId1 = document.getElementById("tempId1");
-            tempId1.remove();
-            var tempId2 = document.getElementById("tempId2");
-            tempId2.remove();
-            var tempId3 = document.getElementById("tempId3");
-            tempId3.remove();
-            var tempId4 = document.getElementById("tempId4");
-            tempId4.remove();
-
-            var windId0 = document.getElementById("windId0");
-            windId0.remove();
-            var windId1 = document.getElementById("windId1");
-            windId1.remove();
-            var windId2 = document.getElementById("windId2");
-            windId2.remove();
-            var windId3 = document.getElementById("windId3");
-            windId3.remove();
-            var windId4 = document.getElementById("windId4");
-            windId4.remove();
-
-            var humidityId0 = document.getElementById("humidityId0");
-            humidityId0.remove();
-            var humidityId1 = document.getElementById("humidityId1");
-            humidityId1.remove();
-            var humidityId2 = document.getElementById("humidityId2");
-            humidityId2.remove();
-            var humidityId3 = document.getElementById("humidityId3");
-            humidityId3.remove();
-            var humidityId4 = document.getElementById("humidityId4");
-            humidityId4.remove();
+            renderFutureData();
+            removeClasses();
         }) 
     };
 })
 
-
-function renderData(){
+// Second call made to Open weather to get UVI and 5 day forecast data
+function renderFutureData(){
 var dataObject = JSON.parse(localStorage.getItem("data"))
 console.log(dataObject);
 getFiveDayForecast();
@@ -119,6 +61,7 @@ function getFiveDayForecast(){
     .then(function(response2){
         return response2.json()
     })
+    // Depending on UVI number background changes to red, green or yellow 
     .then(function(data2){
         console.log(data2);
         displayUV.textContent = "UV Index: " + data2.current.uvi;
@@ -130,9 +73,7 @@ function getFiveDayForecast(){
         } else {
             displayUV.classList.add("moderate"); 
         }
-
-
-
+// For loop used to create all the five day forecast HTML elements
         for (var i = 0; i < 5; i++){
 
             var date5 = document.createElement("h3");
@@ -145,10 +86,8 @@ function getFiveDayForecast(){
             temp5.setAttribute("id", "tempId" + [i])
             wind5.setAttribute("id", "windId" + [i])
             humidity5.setAttribute("id", "humidityId" + [i])
-            const milliseconds2 = (data2.daily[i].dt * 1000);
-            const dateObject2 = new Date(milliseconds2);
-            const dateFormat2 = dateObject2.toLocaleString();
-            date5.textContent = dateFormat2;
+            var dateString2 = moment.unix(data2.daily[i].dt).format("MM/DD/YYYY")
+            date5.textContent = dateString2;
             img5.src = "http://openweathermap.org/img/wn/" + data2.daily[i].weather[0].icon + "@2x.png"
             temp5.textContent = "Temp: " + data2.daily[i].temp.day + "c";
             wind5.textContent = "Wind: " + data2.daily[i].wind_speed + " m/s";
@@ -164,7 +103,7 @@ function getFiveDayForecast(){
 }
 }
 
-
+//Is called when the users prior city choice buttons are pressed and then brings back that data in same way as when they pushed search button
 function nextSearch(event){
     event.preventDefault();
     console.log("correct")
@@ -181,75 +120,20 @@ function nextSearch(event){
     })
     .then(function (data) {
         console.log(data);
-        const milliseconds = (data.dt * 1000)
-        const dateObject = new Date(milliseconds);
-        const dateFormat = dateObject.toLocaleString();
-        displayCity.textContent = data.name + " " + dateFormat
+        var dateString3 = moment.unix(data.dt).format("MM/DD/YYYY")
+        displayCity.textContent = data.name + " " + dateString3
         weatherIcon.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
         displayTemp.textContent = "Temperature: " + Math.round((data.main.temp - 273)) + "c"
         displayWind.textContent = "Wind: " + data.wind.speed + "m/s";
         localStorage.setItem("data", JSON.stringify(data));
-        renderData2();
-//Removes all old 5-day forecast elements to make way for new ones
-        var dateId0 = document.getElementById("dateId0");
-        dateId0.remove();
-        var dateId1 = document.getElementById("dateId1");
-        dateId1.remove();
-        var dateId2 = document.getElementById("dateId2");
-        dateId2.remove();
-        var dateId3 = document.getElementById("dateId3");
-        dateId3.remove();
-        var dateId4 = document.getElementById("dateId4");
-        dateId4.remove(); 
-
-        var imgId0 = document.getElementById("imgId0");
-        imgId0.remove();
-        var imgId1 = document.getElementById("imgId1");
-        imgId1.remove();
-        var imgId2 = document.getElementById("imgId2");
-        imgId2.remove();
-        var imgId3 = document.getElementById("imgId3");
-        imgId3.remove();
-        var imgId4 = document.getElementById("imgId4");
-        imgId4.remove();
-
-        var tempId0 = document.getElementById("tempId0");
-        tempId0.remove();
-        var tempId1 = document.getElementById("tempId1");
-        tempId1.remove();
-        var tempId2 = document.getElementById("tempId2");
-        tempId2.remove();
-        var tempId3 = document.getElementById("tempId3");
-        tempId3.remove();
-        var tempId4 = document.getElementById("tempId4");
-        tempId4.remove();
-
-        var windId0 = document.getElementById("windId0");
-        windId0.remove();
-        var windId1 = document.getElementById("windId1");
-        windId1.remove();
-        var windId2 = document.getElementById("windId2");
-        windId2.remove();
-        var windId3 = document.getElementById("windId3");
-        windId3.remove();
-        var windId4 = document.getElementById("windId4");
-        windId4.remove();
-
-        var humidityId0 = document.getElementById("humidityId0");
-        humidityId0.remove();
-        var humidityId1 = document.getElementById("humidityId1");
-        humidityId1.remove();
-        var humidityId2 = document.getElementById("humidityId2");
-        humidityId2.remove();
-        var humidityId3 = document.getElementById("humidityId3");
-        humidityId3.remove();
-        var humidityId4 = document.getElementById("humidityId4");
-        humidityId4.remove();
+        renderFutureData2();
+        removeClasses();
     }) 
 };
  }
 
- function renderData2(){
+// Renders the five day forecast data when user presses on their prior city searches button
+ function renderFutureData2(){
     var dataObject = JSON.parse(localStorage.getItem("data"))
     console.log(dataObject);
     getFiveDayForecast();
@@ -272,7 +156,6 @@ function nextSearch(event){
                 displayUV.classList.add("moderate"); 
             }
     
-    
             for (var i = 0; i < 5; i++){
                 var date5 = document.createElement("h3");
                 var img5 = document.createElement("img")
@@ -284,10 +167,8 @@ function nextSearch(event){
                 temp5.setAttribute("id", "tempId" + [i])
                 wind5.setAttribute("id", "windId" + [i])
                 humidity5.setAttribute("id", "humidityId" + [i])
-                const milliseconds2 = (data2.daily[i].dt * 1000);
-                const dateObject2 = new Date(milliseconds2);
-                const dateFormat2 = dateObject2.toLocaleString();
-                date5.textContent = dateFormat2;
+                var dateString4 = moment.unix(data2.daily[i].dt).format("MM/DD/YYYY")
+                date5.textContent = dateString4;
                 img5.src = "http://openweathermap.org/img/wn/" + data2.daily[i].weather[0].icon + "@2x.png"
                 temp5.textContent = "Temp: " + data2.daily[i].temp.day + "c";
                 wind5.textContent = "Wind: " + data2.daily[i].wind_speed + " m/s";
@@ -303,3 +184,59 @@ function nextSearch(event){
     }
     }
     
+function removeClasses(){
+    var dateId0 = document.getElementById("dateId0");
+    dateId0.remove();
+    var dateId1 = document.getElementById("dateId1");
+    dateId1.remove();
+    var dateId2 = document.getElementById("dateId2");
+    dateId2.remove();
+    var dateId3 = document.getElementById("dateId3");
+    dateId3.remove();
+    var dateId4 = document.getElementById("dateId4");
+    dateId4.remove(); 
+
+    var imgId0 = document.getElementById("imgId0");
+    imgId0.remove();
+    var imgId1 = document.getElementById("imgId1");
+    imgId1.remove();
+    var imgId2 = document.getElementById("imgId2");
+    imgId2.remove();
+    var imgId3 = document.getElementById("imgId3");
+    imgId3.remove();
+    var imgId4 = document.getElementById("imgId4");
+    imgId4.remove();
+
+    var tempId0 = document.getElementById("tempId0");
+    tempId0.remove();
+    var tempId1 = document.getElementById("tempId1");
+    tempId1.remove();
+    var tempId2 = document.getElementById("tempId2");
+    tempId2.remove();
+    var tempId3 = document.getElementById("tempId3");
+    tempId3.remove();
+    var tempId4 = document.getElementById("tempId4");
+    tempId4.remove();
+
+    var windId0 = document.getElementById("windId0");
+    windId0.remove();
+    var windId1 = document.getElementById("windId1");
+    windId1.remove();
+    var windId2 = document.getElementById("windId2");
+    windId2.remove();
+    var windId3 = document.getElementById("windId3");
+    windId3.remove();
+    var windId4 = document.getElementById("windId4");
+    windId4.remove();
+
+    var humidityId0 = document.getElementById("humidityId0");
+    humidityId0.remove();
+    var humidityId1 = document.getElementById("humidityId1");
+    humidityId1.remove();
+    var humidityId2 = document.getElementById("humidityId2");
+    humidityId2.remove();
+    var humidityId3 = document.getElementById("humidityId3");
+    humidityId3.remove();
+    var humidityId4 = document.getElementById("humidityId4");
+    humidityId4.remove();
+}
