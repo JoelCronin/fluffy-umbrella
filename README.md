e3b664bee2af79142ecb9595563be332d9df2350
joel cronin <you@example.com>
2022-05-31 15:24:12 -0400

    Changed the way the five day forecast HTML elements were populated so that they run in a row not a colum, also added README file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# fluffy-umbrella
+# Weather Forecast
+
+In this task the client wnated me to produce a weather forecast application that could detail the weather for all the major cities across the world. They wanted it to not only have current weather conditions but also a five day forecast, to show the UVI rating(which changes colour depending on its severity) and the ability for the user to research a city without retyping out the city they searched for before.
+
+## Approach
+
+To get the weather data I had to make a fetch request from the Open Weather API. To do this I used the users input to alter the url request that was sent to Open Weather which then responded with a range of current weather data. Pakrt of this was also the cities co-ordinates which I could then use to do another call back to Open Weather 2.5 which provides the UVI figures as well as the five-day forecast.
+
+I then used an if else statement which changed the colour of the UVI Index background depending on its severity and a for loop to iterate through the receiced data and produce the five different days forecasts. Lastly, I added a function in that produced new buttons every time a new city was searched and when pressed the city that was assigned to that button would display its weather forecast data on screen again.
+
+## Final Product
+ 
+ ![Fullsize webpage](./assets/images/Final%20Product.png)
+
+ ## Link
+
+To view the portfolio yourself please [click here](https://joelcronin.github.io/fluffy-umbrella/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,7 @@ var fivedayBox = document.getElementById("five-day-box")
 var secondSearch = document.getElementsByClassName("secondSearch")
 var weatherIcon = document.getElementById("weather-icon")
 
+
 //User Input into search box is used to get weather data from Open Weather when search button is pressed
 searchButton.addEventListener('click', function(event){
     event.preventDefault();
@@ -44,7 +45,6 @@ searchButton.addEventListener('click', function(event){
             cityList.append(cityButton);
             localStorage.setItem("data", JSON.stringify(data));
             renderFutureData();
-            removeClasses();
         }) 
     };
 })
@@ -73,37 +73,20 @@ function getFiveDayForecast(){
         } else {
             displayUV.classList.add("moderate"); 
         }
-// For loop used to create all the five day forecast HTML elements
+// For loop used to populate all the five day forecast HTML elements
         for (var i = 0; i < 5; i++){
-
-            var date5 = document.createElement("h3");
-            var img5 = document.createElement("img")
-            var temp5 = document.createElement("p")
-            var wind5 = document.createElement("p")
-            var humidity5 = document.createElement("p")
-            date5.setAttribute("id", "dateId" + [i])
-            img5.setAttribute("id", "imgId" + [i])
-            temp5.setAttribute("id", "tempId" + [i])
-            wind5.setAttribute("id", "windId" + [i])
-            humidity5.setAttribute("id", "humidityId" + [i])
             var dateString2 = moment.unix(data2.daily[i].dt).format("MM/DD/YYYY")
-            date5.textContent = dateString2;
-            img5.src = "http://openweathermap.org/img/wn/" + data2.daily[i].weather[0].icon + "@2x.png"
-            temp5.textContent = "Temp: " + data2.daily[i].temp.day + "c";
-            wind5.textContent = "Wind: " + data2.daily[i].wind_speed + " m/s";
-            humidity5.textContent = "Humidity: " + data2.daily[i].humidity + "%"
-            fivedayBox.append(date5);
-            fivedayBox.append(img5);
-            fivedayBox.append(temp5);
-            fivedayBox.append(wind5);
-            fivedayBox.append(humidity5);
+            document.getElementById(`date${i}`).textContent = dateString2;
+            document.getElementById(`img${i}`).src = "http://openweathermap.org/img/wn/" + data2.daily[i].weather[0].icon + "@2x.png"
+            document.getElementById(`temp${i}`).textContent = "Temp: " + data2.daily[i].temp.day + "c";
+            document.getElementById(`wind${i}`).textContent = "Wind: " + data2.daily[i].wind_speed + " m/s";
+            document.getElementById(`humidity${i}`).textContent = "Humidity: " + data2.daily[i].humidity + "%"
         }
     })
-    
 }
 }
 
-//Is called when the users prior city choice buttons are pressed and then brings back that data in same way as when they pushed search button
+//Is called when the users prior city choice buttons are pressed and then brings back that data in same way as when they pushed search button except uses button id rather than user text input
 function nextSearch(event){
     event.preventDefault();
     console.log("correct")
@@ -127,9 +110,8 @@ function nextSearch(event){
         displayWind.textContent = "Wind: " + data.wind.speed + "m/s";
         localStorage.setItem("data", JSON.stringify(data));
         renderFutureData2();
-        removeClasses();
     }) 
-};
+    };
  }
 
 // Renders the five day forecast data when user presses on their prior city searches button
@@ -157,86 +139,14 @@ function nextSearch(event){
             }
     
             for (var i = 0; i < 5; i++){
-                var date5 = document.createElement("h3");
-                var img5 = document.createElement("img")
-                var temp5 = document.createElement("p")
-                var wind5 = document.createElement("p")
-                var humidity5 = document.createElement("p")
-                date5.setAttribute("id", "dateId" + [i])
-                img5.setAttribute("id", "imgId" + [i])
-                temp5.setAttribute("id", "tempId" + [i])
-                wind5.setAttribute("id", "windId" + [i])
-                humidity5.setAttribute("id", "humidityId" + [i])
-                var dateString4 = moment.unix(data2.daily[i].dt).format("MM/DD/YYYY")
-                date5.textContent = dateString4;
-                img5.src = "http://openweathermap.org/img/wn/" + data2.daily[i].weather[0].icon + "@2x.png"
-                temp5.textContent = "Temp: " + data2.daily[i].temp.day + "c";
-                wind5.textContent = "Wind: " + data2.daily[i].wind_speed + " m/s";
-                humidity5.textContent = "Humidity: " + data2.daily[i].humidity + "%"
-                fivedayBox.append(date5);
-                fivedayBox.append(img5);
-                fivedayBox.append(temp5);
-                fivedayBox.append(wind5);
-                fivedayBox.append(humidity5);
+                var dateString2 = moment.unix(data2.daily[i].dt).format("MM/DD/YYYY")
+                document.getElementById(`date${i}`).textContent = dateString2;
+                document.getElementById(`img${i}`).src = "http://openweathermap.org/img/wn/" + data2.daily[i].weather[0].icon + "@2x.png"
+                document.getElementById(`temp${i}`).textContent = "Temp: " + data2.daily[i].temp.day + "c";
+                document.getElementById(`wind${i}`).textContent = "Wind: " + data2.daily[i].wind_speed + " m/s";
+                document.getElementById(`humidity${i}`).textContent = "Humidity: " + data2.daily[i].humidity + "%"
             }
         })
-        
     }
     }
     
-function removeClasses(){
-    var dateId0 = document.getElementById("dateId0");
-    dateId0.remove();
-    var dateId1 = document.getElementById("dateId1");
-    dateId1.remove();
-    var dateId2 = document.getElementById("dateId2");
-    dateId2.remove();
-    var dateId3 = document.getElementById("dateId3");
-    dateId3.remove();
-    var dateId4 = document.getElementById("dateId4");
-    dateId4.remove(); 
-
-    var imgId0 = document.getElementById("imgId0");
-    imgId0.remove();
-    var imgId1 = document.getElementById("imgId1");
-    imgId1.remove();
-    var imgId2 = document.getElementById("imgId2");
-    imgId2.remove();
-    var imgId3 = document.getElementById("imgId3");
-    imgId3.remove();
-    var imgId4 = document.getElementById("imgId4");
-    imgId4.remove();
-
-    var tempId0 = document.getElementById("tempId0");
-    tempId0.remove();
-    var tempId1 = document.getElementById("tempId1");
-    tempId1.remove();
-    var tempId2 = document.getElementById("tempId2");
-    tempId2.remove();
-    var tempId3 = document.getElementById("tempId3");
-    tempId3.remove();
-    var tempId4 = document.getElementById("tempId4");
-    tempId4.remove();
-
-    var windId0 = document.getElementById("windId0");
-    windId0.remove();
-    var windId1 = document.getElementById("windId1");
-    windId1.remove();
-    var windId2 = document.getElementById("windId2");
-    windId2.remove();
-    var windId3 = document.getElementById("windId3");
-    windId3.remove();
-    var windId4 = document.getElementById("windId4");
-    windId4.remove();
-
-    var humidityId0 = document.getElementById("humidityId0");
-    humidityId0.remove();
-    var humidityId1 = document.getElementById("humidityId1");
-    humidityId1.remove();
-    var humidityId2 = document.getElementById("humidityId2");
-    humidityId2.remove();
-    var humidityId3 = document.getElementById("humidityId3");
-    humidityId3.remove();
-    var humidityId4 = document.getElementById("humidityId4");
-    humidityId4.remove();
-}
